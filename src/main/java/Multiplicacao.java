2c464f54e3fc68c688e407145e32d5775d376e94
victorfm <victormatoscps@gmail.com>
2019-06-11 08:36:35 -0300

    Repetiçao a quantidade da potencia vezes a base.

---
+++ src/main/java/Multiplicacao.java
@@ -0,0 +1,11 @@
+public class Multiplicacao {
+    public static void main(String[] args) {
+        int y = 5, x = 5, result = 1;
+        while (y > 0) {
+            y--;
+            result = result * x;
+        }
+        System.out.println(result);
+
+    }
+}
