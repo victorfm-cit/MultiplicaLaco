3e6962da26e2c933be7ee9903e9d07fb7d7a4895
victorfm <victormatoscps@gmail.com>
2019-06-11 14:21:22 -0300

    Integer.parseInt

--- src/main/java/Multiplicacao.java
+++ src/main/java/Multiplicacao.java
@@ -1,6 +1,6 @@
 public class Multiplicacao {
     public static void main(String[] args) {
-        int y = 5, x = 5, result = 1;
+        int x = Integer.parseInt(args[0]), y = Integer.parseInt(args[1]), result = 1;
         while (y > 0) {
             y--;
             result = result * x;
